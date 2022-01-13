820efb343eb15112830c74069a7e5375231e346b
zbzwf <97572249+zbzwf@users.noreply.github.com>
2022-01-13 10:51:20 +0800

    提交代码

---
+++ test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//用while在屏幕上打印1-10
+//int main()
+//{
+//	int sum = 1;
+//	while (sum <= 10)
+//	{
+//		printf("%d\n",sum);
+//		sum++;
+//	}
+//	return 0;
+//}
+//输出1—100之间的奇数
+//int main()
+//{
+//	int sum = 0;
+//	for (; sum <= 100; sum++)
+//	{
+//		if (0 != sum % 2)
+//			printf("%d\n",sum);
+//	}
+//	return 0;
+//}
+// 
+//判断奇偶
+//int main()
+//{
+//	int sum = 0;
+//	scanf("%d",&sum);
+//	if (0 == sum % 2)
+//		printf("是偶数");
+//	else
+//		printf("是奇数");
+//	return 0;
+//}
