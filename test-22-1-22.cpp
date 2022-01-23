45dfc932272759190aded28a62b8764e3c4ce454
zbzwf <97572249+zbzwf@users.noreply.github.com>
2022-01-23 09:41:22 +0800

    提交代码

---
+++ test-22-1-22.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1	
+#include<stdio.h>
+
+//int main()
+//{与或非，自加操作符
+//	int i = 0, a = 0, b = 2, c = 3, d = 4;
+//	i = a++ && ++b && d++;
+//	printf("%d,%d,%d,%d",a,b,c,d);
+//	a = 1;
+//	i = a++ || ++b || d++;
+//	printf("%d,%d,%d,%d",a,b,c,d);
+//	
+//	return 0;
+//}
+
+//int main()
+//{
+//    //[]数组调用操作符
+//    int arr[10] = { 0 };
+//    arr[4] = 10;
+//    printf("%d", arr[4]);
+//    return 0;
+//}
+
+//int  add(int a, int b)
+//{
+//	return a + b;
+//}
+//int main()//函数引用操作符()
+//{
+//	int a = 10, b = 5, c = 0;
+//	c = add(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
+
