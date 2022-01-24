535191f41f53b66dbb86abb598dbc3d69cdfb336
zbzwf <97572249+zbzwf@users.noreply.github.com>
2022-01-24 15:57:55 +0800

    提交代码

---
+++ test-22-1-23.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1	
+#include<stdio.h>
+
+//初识指针
+//int main()
+//{
+//	printf("%d\n",sizeof(char*));
+//	printf("%d\n", sizeof(short*));
+//	printf("%d\n", sizeof(long long*));
+//	printf("%d\n", sizeof(double*));
+//	return 0;
+//}
+
+//不同指针加减整数
+//int main()
+//{
+//	int a = 0x11223344;
+//	char b = 0x11;
+//	int* pa = &a;
+//	char* pc = &b;
+//	printf("%p\n",pa);
+//	printf("%p\n", pa + 1);
+//	printf("%p\n", pc );
+//	printf("%p\n", pc+ 1);
+//	return 0;
+//}
